6ac2063d6f163c6ebd68929541479ac10fb5d938
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-10-19 12:48:23 -0300

    mi validador para clase de desarrollo web

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Button} from "@material-ui/core"
+import MiComponente from './MiComponente';
+import MiValidador from './MiValidador';
+
 function App() {
   return (
-    <div className="App">
+    <div className="App">n
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Clave uvalpo</p>
+        <MiValidador  valor = "123344" />
       </header>
     </div>
   );
 }
+
+
 
 export default App;
